51ba70dfa7154cccd358db76460ff4a68b453255
larissaleite <larissa.leite@gmail.com>
2014-10-20 19:25:21 -0300

    Restructuring app

--- clientes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from clientes.models import Cliente, Cartao, Prato, Pedido
-
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Cartao)
-admin.site.register(Prato)
-admin.site.register(Pedido)
--- restaurantes/urls.py
+++ restaurantes/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'', include('clientes.urls')), #to support all urls defined in clientes.urls
+    url(r'', include('restaurantes.core.urls')), #to support all urls defined in core.urls
     url(r'^admin/', include(admin.site.urls)),
 )
